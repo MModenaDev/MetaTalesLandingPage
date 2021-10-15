c31db0e182c04407a4bdbc7255643d1c0d753046
Marco Modena <marco.modena.serv@gmail.com>
2021-10-15 09:34:21 -0300

    feat(Tale): created component

---
+++ components/Tale.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+
+type TaleProps = {
+	universe: string;
+	collection: string;
+	type: string;
+	name: string;
+	description: string;
+}
+
+const Tale: React.FC<TaleProps> = ({ universe, collection, type, name, description }) => {
+	return (
+		<div className="flex flex-col gap-4 border border-tertiary border-solid rounded-2xl mr-8 p-4 max-w-md bg-primary">
+			<p className="text-lg"><span className="font-bold text-tertiary">Universe: </span>{universe}</p>
+			<p className="text-lg"><span className="font-bold text-tertiary">Collection: </span>{collection}</p>
+			<p className="text-lg"><span className="font-bold text-tertiary">Type: </span>{type}</p>
+			<p className="text-lg"><span className="font-bold text-tertiary">Name: </span>{name}</p>
+			<p className="text-lg">{description}</p>
+		</div>
+	)
+}
+
+export default Tale;
