b26ffe8d58fbc74b49dc6daf0022b699fdbb422e
Marco Modena <marco.modena.serv@gmail.com>
2021-10-15 09:19:08 -0300

    feat(SectionSponsor): created section

---
+++ components/SectionSponsors.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+
+import Image from 'next/image'
+
+type SectionProductProps = {}
+
+const SectionProduct: React.FC<SectionProductProps> = () => {
+	return (
+		<section className="border-t border-tertiary bg-opacity-75 w-full px-8 py-4 flex justify-center items-center">
+			<a href="http://sandbox.mit.edu/">
+				<Image src="/Sandbox-Logo.png" height={80} width={133} layout="fixed" alt="MIT Sandbox Logo" />
+			</a>
+		</section>
+	)
+}
+
+export default SectionProduct;
