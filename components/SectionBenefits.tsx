3e2b50bc9e3a65685ed52eaaa29f3a9f0cfb0d2d
Marco Modena <marco.modena.serv@gmail.com>
2021-10-21 12:34:26 -0300

    fix(SectionBenefits): updated text & changed name

---
+++ components/SectionBenefits.tsx
@@ -0,0 +1,39 @@
+import * as React from "react";
+
+import Image from 'next/image'
+
+type SectionBenefitsProps = {}
+
+const SectionBenefits: React.FC<SectionBenefitsProps> = () => {
+	return (
+		<section className="w-full px-8 md:px-20 lg:px-40 xl:px-60 py-16 text-center mb-4">
+			<h3 className="text-3xl pb-8"><strong>Benefits</strong></h3>
+			<div className="flex flex-wrap lg:flex-nowrap justify-between gap-8">
+				<div className="flex flex-col items-center">
+					<Image src="/crypto.svg" height={120} width={173} layout="fixed" />
+					<h5 className="text-xl">Be rewarded<br /><span className="text-lg">for contributing</span></h5>
+					<p className="mt-2">
+						If you buy an NFT, you receive exclusive content and perks.
+						If you create one approved by the community, you receive a fair share.
+					</p>
+				</div>
+				<div className="flex flex-col items-center">
+					<Image src="/quality.svg" height={120} width={161} layout="fixed" />
+					<h5 className="text-xl">Be listened<br /><span className="text-lg">about the characters you love</span></h5>
+					<p className="mt-2">
+						No more crappy endings. NFT holders of characters can propose and vote for key decisions (art, stories and licencing)
+					</p>
+				</div>
+				<div className="flex flex-col items-center">
+					<Image src="/organize.svg" height={120} width={142} layout="fixed" />
+					<h5 className="text-xl">Be part<br /><span className="text-lg">of new universes</span></h5>
+					<p className="mt-2">
+						From Cyberpunk to Medieval, help to co-create new universes and engage in new communities
+					</p>
+				</div>
+			</div>
+		</section>
+	)
+}
+
+export default SectionBenefits;
