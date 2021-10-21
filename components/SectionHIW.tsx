bd3644cfac8d9eb19c9915f755fd59fbbee0dd39
Marco Modena <marco.modena.serv@gmail.com>
2021-10-21 12:28:01 -0300

    fix(SextionHIW): updated text, name & removed code

---
+++ components/SectionHIW.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import Image from 'next/image'
+
+type SectionHIWProps = {}
+
+const SectionHIW: React.FC<SectionHIWProps> = () => {
+	return (
+		<section className="w-full px-8 md:px-20 lg:px-40 xl:px-60 py-16 flex flex-col justify-center items-center">
+			<h3 className="text-3xl pb-1"><strong>How it works</strong></h3>
+			<div className="flex flex-col items-center">
+				<h4 className="text-2xl pb-4"><em>Tales: The building blocks for stories in the Metaverse</em></h4>
+				<p className="mb-5">
+					Tales are NFTs (non-fungible tokens) that represent characters, locations and world-building elements, created and managed by communities.
+				</p>
+			</div>
+			<Image src="https://placekitten.com/700/706" width="700" height="706" />
+		</section>
+	)
+}
+
+export default SectionHIW;
