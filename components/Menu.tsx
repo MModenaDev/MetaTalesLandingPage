cadfa443b64f1e1a9a316ae1177df51ffb5b639b
Marco Modena <marco.modena.serv@gmail.com>
2021-10-27 12:02:05 -0300

    feat(): WIP

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import Image from 'next/image'
 
 import Link from 'next/link'
 import { useRouter } from 'next/router';
@@ -7,23 +8,82 @@ type MenuProps = {}
 
 const Menu: React.FC<MenuProps> = () => {
 	const router = useRouter();
+	const [isMobile, setIsMobile] = React.useState(false);
+	const [menuOpen, setMenuOpen] = React.useState(false);
+
+	React.useEffect(() => {
+		if (window.screen.width < 420) setIsMobile(true);
+	})
 
 	const generateClass = (route: string) => {
+		let newClass = "hover:text-MTred transition-colors ";
+		if (isMobile) {
+			newClass += "w-full text-center ";
+		}
 		if (router.asPath === route) {
-			return "hover:text-MTred transition-colors border-b border-MTred"
+			newClass += "border-b border-MTred ";
+		}
+		return newClass;
+	}
+
+	const onSignUp = () => {
+		if (router.asPath === "/") {
+			window.scrollTo(0, document.body.scrollHeight);
 		} else {
-			return "hover:text-MTred transition-colors"
+			router.push("/#signup");
 		}
 	}
 
-	return (
-		<nav className="flex gap-3 md:gap-8 items-center">
+	const Nav = () => (
+		<nav className={`flex flex-wrap gap-3 md:gap-8 items-center ${isMobile ? "w-full" : ""}`}>
 			<Link href="/faq"><a className={generateClass("/faq")}>FAQ</a></Link>
 			{/* <Link href="/manifesto"><a className={generateClass("/manifesto")}>Manifesto</a></Link> */}
 			{/* <Link href="/roadmap"><a className={generateClass("/roadmap")}>Roadmap</a></Link> */}
 			<Link href="/team"><a className={generateClass("/team")}>Team</a></Link>
 		</nav>
+	)
+
+	if (isMobile) {
+		return (
+			<>
+				{!menuOpen ? (
+					<button type="button" onClick={() => setMenuOpen(true)}>
+						<Image src="/menu.svg" width={16} height={16} />
+					</button>
+				) : (
+					<button type="button" onClick={() => setMenuOpen(false)} >
+						<Image src="/close.svg" width={16} height={16} />
+					</button>
+				)}
+				{menuOpen && (
+					<div className="flex flex-wrap gap-4 mt-4 w-full">
+						<Nav />
+						<button
+							className="py-1 px-3 font-bold border border-MTred rounded-2xl bg-MTred text-primary hover:text-MTred hover:bg-primary transition-colors w-full"
+							type="button"
+							onClick={() => onSignUp()}
+						>
+							Sign Up
+						</button>
+					</div>
+				)}
+			</>
+		)
+	}
+
+	return (
+		<div className="flex flex-wrap gap-4">
+			<Nav />
+			<button
+				className="py-1 px-3 font-bold border border-MTred rounded-2xl bg-MTred text-primary hover:text-MTred hover:bg-primary transition-colors"
+				type="button"
+				onClick={() => onSignUp()}
+			>
+				Sign Up
+			</button>
+		</div>
 	);
+
 }
 
 export default Menu;
